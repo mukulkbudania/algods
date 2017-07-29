3144acc5bc4e087f6ff82b170cd578e61f6628ac
mukulkbudania <mukul.k.budania@gmail.com>
2017-07-29 05:30:11 +0530

    Final programs

--- src/prep/interview/geeks/companies/amazon/NumberOfTraingles.java
+++ src/prep/interview/geeks/companies/amazon/NumberOfTraingles.java
@@ -5,6 +5,21 @@ package prep.interview.geeks.companies.amazon;
  */
 public class NumberOfTraingles {
     public static void main(String[] args){
-        
+        int a[] = {4,6,7,8,9};
+        System.out.println(countTriangles(a));
+    }
+
+    private static int countTriangles(int[] a) {
+        int n = a.length;
+        int count=0;
+        for(int i=0;i<n-2;i++){
+            //Move either k or j. j would always be less than k. So O(n)
+            int k=i+2;
+            for(int j=i+1;j<n-1;j++){
+                while (k<n&& a[i]+a[j]>a[k])++k;
+                count+=k-j-1;
+            }
+        }
+        return count;
     }
 }
